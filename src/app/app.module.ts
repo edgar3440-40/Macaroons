19aa530e86a166ca8c3077a7d55aac2215684fcc
Edgar Baghdasaryan <edgarbaghdasaryan15@gmail.com>
2024-03-10 12:20:49 +0200

    mistakes corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,11 @@ import { InputEffectsDirective } from './directives/input-effects.directive';
 import { ShortenTextPipe } from './pipes/shorten-text.pipe';
 import { AdvantageComponent } from './components/advantage/advantage.component';
 import { PhoneFormatPipe } from './pipes/phone-format.pipe';
+
+import {registerLocaleData} from "@angular/common";
+import localeRu from '@angular/common/locales/ru';
+
+registerLocaleData(localeRu)
 
 @NgModule({
   declarations: [
@@ -29,7 +34,18 @@ import { PhoneFormatPipe } from './pipes/phone-format.pipe';
         AppRoutingModule,
         FormsModule,
     ],
-  providers: [ProductService],
+  providers: [
+    ProductService,
+
+    {
+      provide: LOCALE_ID,
+      useValue: 'ru-RU'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'RUB'
+    }
+  ],
   bootstrap: [AppComponent, FooterComponent]
 })
 export class AppModule { }
